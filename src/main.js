e578f98568103e410a02488f5b6c956e38e95796
SaraAndreasen <55578265+SaraAndreasen@users.noreply.github.com>
2024-06-19 01:23:09 +0200

    injected more weather data

--- src/main.js
+++ src/main.js
@@ -1,40 +1,59 @@
-let weekDays = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-
-let currentTime = document.querySelector("#date");
-let date = new Date();
-
 let apiKey = "c3tf38bfdaaca9c9406e786o84b5cbf8";
-
-let weekDay = weekDays[date.getDay()];
-let currentHour = date.getHours();
-let currentMinute = date.getMinutes();
-
-currentTime.innerHTML = `${weekDay} ${currentHour}:${currentMinute}`;
-
-// Display searched city
-function capitalizeCityName(city) {
-  return city.replace(/\b\w/g, function (char) {
-    return char.toUpperCase();
-  });
-}
-let cityInput = document.querySelector("#search-input-city");
-let cityName = document.querySelector("#city-name");
-let currentTempHeading = document.querySelector("#current-temperature");
 
 // API response
 function displayCityAndTemp(response) {
+  let cityName = document.querySelector("#city-name");
+  let currentTemperatureElement = document.querySelector(
+    "#current-temperature"
+  );
+  let currentTimeElement = document.querySelector("#date");
+  let weatherConditionsElement = document.querySelector("#weather-conditions");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+
   let currentTemp = Math.round(response.data.temperature.current);
   let cityFromResponse = response.data.city;
-  currentTempHeading.innerHTML = `${currentTemp}`;
+  let weatherConditionsFromCity = response.data.condition.description;
+  let date = new Date(response.data.time * 1000);
+  let currentLocalHumidity = response.data.temperature.humidity;
+  let currentWindSpeed = response.data.wind.speed;
+
+  console.log(response.data.time);
+  console.log(response.data);
+
+  weatherConditionsElement.innerHTML = `${weatherConditionsFromCity}`;
+  currentTemperatureElement.innerHTML = `${currentTemp}`;
   cityName.innerHTML = `${cityFromResponse}`;
+  humidityElement.innerHTML = `${currentLocalHumidity}%`;
+  windElement.innerHTML = `${currentWindSpeed} km/h`;
+  currentTimeElement.innerHTML = formatDate(date);
+
+  console.log(response.data.time);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+
+  let weekDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let day = weekDays[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
@@ -44,6 +63,7 @@ function searchCity(city) {
 }
 
 function handleSearchSubmit(event) {
+  let cityInput = document.querySelector("#search-input-city");
   event.preventDefault();
 
   searchCity(cityInput.value);
